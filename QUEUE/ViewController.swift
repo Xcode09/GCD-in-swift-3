ba89102a5eb75b3e7d11f12f4e9e3c7e68a1af01
Xcode09 <alisaleem877@gmail.com>
2017-03-30 22:11:16 +0500

    GCD in swift 3
    Source code for Grand central Dispatch
    It is also called current programming…

--- QUEUE/ViewController.swift
+++ QUEUE/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Concurrent Programming With GCD In Swift 3
+        GCD()
+        
     }
+    func GCD(){
+        let Queue = DispatchQueue(label: "myvsdvfs",qos:.userInteractive)
+        let Queue1 = DispatchQueue(label: "ALI.developer.com",qos:.background)
+        
+        Queue.async {
+            for i in 90...1000{
+                print("😎",i)
+            }
+            
+        }//End of first queue
+        Queue1.async {
+            for i in 0...1000{
+                print("😇",i)
+            }
+        }//End of second queue
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
